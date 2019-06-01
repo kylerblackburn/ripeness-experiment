96b08e69569cc3388271b2b4e1404cd4a0c95b60
kylerblackburn <0397540@schoolsnet.act.edu.au>
2019-06-01 16:31:46 +1000

    script working perfectly for bananas, nearly for tomatoes

--- bin/process-one.sh
+++ bin/process-one.sh
@@ -56,32 +56,39 @@ x=$(($xoff + ($col * $xpitch)))
 convert $in -crop $xpitch"x2000+"$x+$y $out
 
 #find the bounding box
-convert $out -fuzz 15% -fill white -opaque '#b8b8b8' $outdir/$base-$id-nogray.png
-convert $outdir/$base-$id-nogray.png +dither -colors 5 $outdir/$base-$id-dither.png
+#convert $out -fuzz 15% -fill white -opaque '#b8b8b8' $outdir/$base-$id-nogray.png
+#convert $outdir/$base-$id-nogray.png +dither -colors 5 $outdir/$base-$id-dither.png
+convert $out -fuzz 10% -transparent '#b8b8b8' -fuzz 10% -transparent '#707070' -fuzz 10% -transparent '#a0a0a0' -fuzz 10% -transparent '#d0d0d0' -fuzz 10% -transparent white -fuzz 10% $outdir/$base-$id-crop.png
 xmid=$(( $xpitch / 2 ))
 ymid=1000
-$DIR/magicwand.sh $xmid,$ymid -t 25 -c transparent $outdir/$base-$id-dither.png $outdir/$base-$id-ditherwand.png
-convert $outdir/$base-$id-ditherwand.png -trim $outdir/$base-$id-crop.png
+$DIR/magicwand.sh $xmid,$ymid -t 25 -c transparent $outdir/$base-$id-crop.png $outdir/$base-$id-wand.png
+convert $outdir/$base-$id-wand.png -trim $outdir/$base-$id-crop.png
+width=`convert $outdir/$base-$id-crop.png -format "%w" info:`
+height=`convert $outdir/$base-$id-crop.png -format "%h" info:`
+ulx=`convert $outdir/$base-$id-crop.png -format "%X" info:`
+uly=`convert $outdir/$base-$id-crop.png -format "%Y" info:`
+ulxabs=`convert $outdir/$base-$id-crop.png -format "%[fx:$ulx+$x]" info:`
+ulyabs=`convert $outdir/$base-$id-crop.png -format "%[fx:$uly+$y]" info:`
+# echo "ulx=$ulx; uly=$uly; ulxabs=$ulxabs; ulyabs=$ulyabs;"
+cr=$width"x"$height"+"$ulxabs"+"$ulyabs
+# echo "cr=$cr"
+convert $out -crop $cr -fuzz 10% -transparent '#b8b8b8' -fuzz 10% -transparent '#707070' -fuzz 10% -transparent '#a0a0a0' -fuzz 10% -transparent '#d0d0d0' -fuzz 10% -transparent white -fuzz 10% $outdir/$base-$id-crop.png
+xmid=$(( $width/3 )) #don't take middle because bend in bananas
+ymid=$(( $height/2 ))
+$DIR/magicwand.sh $xmid,$ymid -t 50 -c transparent $outdir/$base-$id-crop.png $outdir/$base-$id-wand.png
+convert $outdir/$base-$id-wand.png +dither -colors 4 $outdir/$base-$id-dither.png
+convert $outdir/$base-$id-crop.png -gravity center -background transparent -extent 1250x1250 $outdir/$base-$id-sq.png
+convert $outdir/$base-$id-wand.png -scale 1x1! -alpha off $outdir/$base-$id-1x1.png;
+echo -n "$base-$id "
+convert $outdir/$base-$id-1x1.png -format "%[pixel:u.p]" info:;
+echo;
+convert $outdir/$base-$id-1x1.png -scale 100x100! $outdir/$base-$id-100x100.png;
+
+exit
 
 #convert $out -fuzz 10% -transparent '#b8b8b8' -fuzz 10% -transparent '#707070' -fuzz 10% -transparent '#a0a0a0' -fuzz 10% -transparent '#d0d0d0' -fuzz 10% -transparent white -trim $outdir/$base-$id-crop.png;
 # convert $out -fuzz 10% -transparent '#b8b8b8' -fuzz 10% -transparent '#707070' -fuzz 10% -transparent '#a0a0a0' -fuzz 10% -transparent '#d0d0d0' -fuzz 10% -transparent white -fuzz 10% -transparent '#342427' -fuzz 10% -transparent '#505050' -trim $outdir/$base-$id-crop.png;
 #convert $out -fuzz 30% -transparent black -fuzz 33% -transparent '#b8b8b8' -fuzz 10% -trim $outdir/$base-$id-crop.png;
 
-width=`convert $outdir/$base-$id-crop.png -format "%w" info:`
-height=`convert $outdir/$base-$id-crop.png -format "%h" info:`
-ulx=`convert $outdir/$base-$id-crop.png -format "%X" info:`
-uly=`convert $outdir/$base-$id-crop.png -format "%Y" info:`
-brx=`convert $outdir/$base-$id-crop.png -format "%[fx:$ulx+$width-1]" info:`
-bry=`convert $outdir/$base-$id-crop.png -format "%[fx:$uly+$height-1]" info:`
-# echo "ulx=$ulx; uly=$uly; brx=$brx; bry=$bry;"
-cr=$width"x"$height$ulx$uly
 # echo $cr
 #convert $in -crop $cr -fuzz 50% -transparent white $outdir/$base-$id-crop.png
-# convert $in -crop $cr -fuzz 10% -transparent '#b8b8b8' -fuzz 10% -transparent '#707070' -fuzz 10% -transparent '#a0a0a0' -fuzz 10% -transparent '#d0d0d0' -fuzz 10% -transparent white -fuzz 10% $outdir/$base-$id-crop.png
-
-convert  $outdir/$base-$id-crop.png -gravity center -background transparent -extent 1250x1250 $outdir/$base-$id-sq.png
-convert $outdir/$base-$id-crop.png -scale 1x1! -alpha off $outdir/$base-$id-1x1.png;
-echo -n "$base-$id "
-convert $outdir/$base-$id-1x1.png -format "%[pixel:u.p]" info:;
-echo;
-convert $outdir/$base-$id-1x1.png -scale 100x100! $outdir/$base-$id-100x100.png;
